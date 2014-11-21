e9e277290d9a841980f872ad78d7fca22dc8af44
Eric Rumble <eric.rumble@sage.com>
2014-11-21 13:54:13 -0500

    remove packer templates from this repo

--- packer/scripts/common/vagrant.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir ~vagrant/.ssh
-curl -o ~vagrant/.ssh/authorized_keys \
-    https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
-chown -R vagrant ~vagrant/.ssh
-chmod -R go-rwsx ~vagrant/.ssh
